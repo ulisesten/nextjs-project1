b458ef3db9846ff5d68c5224e664e9bebe6b8f4a
Ulises <ulises.martinez@dicex.com>
2024-03-06 22:38:47 -0600

    reescritura completa

---
+++ pages/generales/componentes/Nav.js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import estilo from '../estilos/generales.module.css'
+
+const Nav = () => {
+  return (
+    <nav className={estilo.nav}>
+      <ul className={estilo.ul_nav}>
+        <li className={estilo.li_nav}>
+            <Link href="/" className={estilo.link_nav}>
+                <img src="./assets/Aventura.png" className={estilo.img_nav} alt="Carrera Aventura" />
+                <img src="./assets/CARRERAS.png" className={estilo.img_nav} alt="CARRERAS" />
+                <span className="">Carrera Aventura</span>
+            </Link>
+        </li>
+        <li className={estilo.li_nav}>
+            <Link href="/carreras" className={estilo.link_nav}>
+                Carreras
+            </Link>
+        </li>
+        <li className={estilo.li_nav}>
+            <Link href="/historico" className={estilo.link_nav}>
+                Histórico
+            </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './generales/componentes/Nav';
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Nav />
+      <p>Esta es la página de inicio de mi aplicación Next.js.</p>
+    </div>
+  );
+};
+
+export default HomePage;
