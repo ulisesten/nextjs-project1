a47d34e5fb18d5ab8082a6f38e0f12ed6c4e80ff
Ulises <ulises.martinez@dicex.com>
2024-03-07 08:09:29 -0600

    Señadió página carreras

---
+++ pages/carreras/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../generales/componentes/Nav';
+
+
+const CarrerasPage = () => {
+  return (
+    <div>
+      <Nav />
+      <p>Esta es la página de Carreras</p>
+    </div>
+  );
+};
+
+export default CarrerasPage;
--- pages/generales/componentes/Nav.js
+++ pages/generales/componentes/Nav.js
@@ -6,21 +6,27 @@ const Nav = () => {
     <nav className={estilo.nav}>
       <ul className={estilo.ul_nav}>
         <li className={estilo.li_nav}>
+
             <Link href="/" className={estilo.link_nav}>
                 <img src="./assets/Aventura.png" className={estilo.img_nav} alt="Carrera Aventura" />
                 <img src="./assets/CARRERAS.png" className={estilo.img_nav} alt="CARRERAS" />
                 <span className="">Carrera Aventura</span>
             </Link>
+
         </li>
         <li className={estilo.li_nav}>
+
             <Link href="/carreras" className={estilo.link_nav}>
                 Carreras
             </Link>
+
         </li>
         <li className={estilo.li_nav}>
+
             <Link href="/historico" className={estilo.link_nav}>
                 Histórico
             </Link>
+            
         </li>
       </ul>
     </nav>
